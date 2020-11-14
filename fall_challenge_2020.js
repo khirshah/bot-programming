50f0c0368808599977f5293a7204e1668f71432e
khirshah <khirshah@gmail.com>
2020-11-14 03:41:34 +0000

    handling time and rating added, sorting order is toggleable, now recipes are ordered by rating (money/round)

--- fall_challenge_2020.js
+++ fall_challenge_2020.js
@@ -1,27 +1,32 @@
-const playRound = (recipes, myBag, spells) => {
+const playRound = (recipes, myBag, spellbook, tome) => {
     let action;
     //sort recipes by reward
-    const sortedRecipes = sortRecipes(recipes);
+    const ratedRecipes = rateRecipes(recipes, myBag.resources);
+    //possible sort orders are: rating and price at the moment
+    const sortedRecipes = sortRecipesBy(ratedRecipes, 'rating');
+    //console.error('tome: ', tome, 'spellbook: ',spellbook);
+    //console.error(sortedRecipes);
     //check is any of them can be brewed
-    const bestAffordableRecipe = canAffordBrew(sortedRecipes, myBag.resources);
-    if (bestAffordableRecipe === 'NONE') {
+    const potionToBrew = canAffordBrew(sortedRecipes[0], myBag.resources);
+    console.error(potionToBrew);
+    if (potionToBrew === 'NONE') {
         //none of them are affordable so let's pick the one with the highest reward
         const chosenRecipe = sortedRecipes[0];
         //select a spell to cast
-        const spellToCast = useMagic(spells, myBag.resources, chosenRecipe);
+        const spellToCast = useMagic(spellbook, myBag.resources, chosenRecipe);
         if (spellToCast === 'NONE') {
             action = 'REST';
         } else {
             action = `CAST ${spellToCast}`
         }
     } else {
-        action = `BREW ${bestAffordableRecipe}`
+        action = `BREW ${potionToBrew}`
     }
-    console.error(action)
+    console.error(action);
     return action;
 }
 
-//---------------------- cast ----------------------------------------
+//---------------------- spells ----------------------------------------
 const canCast = (spell) => {
     if (spell.castable) {
         return spell.id;
@@ -30,61 +35,105 @@ const canCast = (spell) => {
     }
 }
 
-const conjureResource = (i, myResources, spells) => {
+const conjureResource = (i, myResources, spellbook) => {
     if (i === 0) {
         //its a blue ingredient
         //check if +2 blue spell is available
-        const spell = spells[0]
+        const spell = spellbook[0]
         return canCast(spell);
     } else {
         //it's a higher level ingredient
-        console.error(i  -1, myResources);
         if (myResources[i - 1] > 0) {
             //we have the resource to conjure it from
             //try casting the appropriate spell
-            const spell = spells[i]
+            const spell = spellbook[i]
             return canCast(spell);
         } else {
             //we don't have the resource to conjure it from
             //try with lower level ingredient
-            return conjureResource(i-1, myResources, spells);
+            return conjureResource(i-1, myResources, spellbook);
         }
     }
 }
 
-const useMagic = (spells, myResources, recipe) => {
+const useMagic = (spellbook, myResources, recipe) => {
     for (i = 0; i < recipe.ingredients.length; ++i) {
         //compare resource and ingredient requirement
         if (myResources[i] < Math.abs(recipe.ingredients[i])) {
             //don't have the required amount
-            return conjureResource(i, myResources, spells);
+            return conjureResource(i, myResources, spellbook);
         }
     }
     return 'NONE';
 }
 
-//---------------------- Brew ----------------------------------------
-const sortRecipes = (recipeList) => {
-    const sortedByPrice = recipeList.sort((a, b) => b.price - a.price);
-    return sortedByPrice;
+//---------------------- recipes ----------------------------------------
+const rateRecipes = (recipeList, resourceList) => {
+    const recipes = recipeList;
+    const ratedRecipes = recipes.map(recipe => {
+        const handlingTime = calculateHandlingTime(recipe.ingredients, resourceList);
+        const rating = recipe.price/handlingTime;
+        updatedRecipe = {
+            ...recipe,
+            handlingTime,
+            rating
+        };
+        return updatedRecipe;
+    })
+    return ratedRecipes;
 }
 
-const canAffordBrew = (recipes, myIngredients) => {
-    for (recipe of recipes) {
-        let haveAllIngredients = true;
-        //loop through types of ingredients
-        for (i in recipe.ingredients) {
-            //compare bag content and requirement
-            if (myIngredients[i] < Math.abs(recipe.ingredients[i])) {
-                haveAllIngredients = false;
-            }
+const calculateHandlingTime = (ingredientList, resourceList) => {
+    //the resource's place in the array defines the level of the item
+    const individualTimes = ingredientList.map((item, level) => {
+        const requriedAmount = Math.abs(item);
+        const availableamount = resourceList[level];
+        const amountNeeded = cantBeNegative(requriedAmount - availableamount);
+        if (level === 0) {
+            //blue runes
+            //assumes: +2 blue spell is available, max 4 blue runes are required (when amount > 2)
+            return amountNeeded <= 2 ? 1 : 3;
+        } else {
+            //higher level runes
+            //the number of items times their levels (with one level at a time transformation)
+            //+1 for the rest action
+            return Math.abs(amountNeeded)*level+1;
         }
-        //return with recipe id if we have everything
-        if (haveAllIngredients) {
-            return recipe.id;
+    });
+    const handlingTime = sum(individualTimes);
+    return handlingTime;
+}
+
+
+const sortRecipesBy = (recipeList, order) => {
+    switch (order) {
+        case 'rating':
+            return recipeList.sort((a, b) => b.rating - a.rating);
+
+        case 'price':
+            return recipeList.sort((a, b) => b.price - a.price);
+    
+        default:
+            return recipeList;
+    }
+}
+
+const canAffordBrew = (recipe, resourceList) => {
+    const ingredientList = recipe.ingredients;
+    let haveAllIngredients = true;
+    //loop through types of ingredients
+    for (i in ingredientList) {
+        //compare bag content and requirement
+        if (resourceList[i] < Math.abs(ingredientList[i])) {
+            haveAllIngredients = false;
         }
     }
-    return 'NONE';
+    //return with recipe id if we have everything
+    if (haveAllIngredients) {
+        return recipe.id;
+    } else {
+        return 'NONE';
+    }
 };
 
 //---------------------- helpers ----------------------------------------
@@ -99,3 +148,7 @@ const absSum = (array) => {
         return Math.abs(a) + Math.abs(b);
     }, 0);
 };
+
+const cantBeNegative = (number) => {
+    return number < 0 ? 0 : number;
+}
